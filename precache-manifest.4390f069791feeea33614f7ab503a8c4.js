6b058db73c0404821f224befc71546b3ac22b1ce
Milind Nayse <milindnayse@gmail.com>
2019-04-25 14:32:46 +0530

    Updates

---
+++ precache-manifest.4390f069791feeea33614f7ab503a8c4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61ff380e5103f0db8cca0fa93cac696d",
+    "url": "/good-reads/index.html"
+  },
+  {
+    "revision": "d662e93a50c17515e176",
+    "url": "/good-reads/static/css/main.0e6e9412.chunk.css"
+  },
+  {
+    "revision": "03222439fc7b805946a0",
+    "url": "/good-reads/static/js/2.7d972699.chunk.js"
+  },
+  {
+    "revision": "d662e93a50c17515e176",
+    "url": "/good-reads/static/js/main.e58677f1.chunk.js"
+  },
+  {
+    "revision": "7b42e0c6c32a4329f11a",
+    "url": "/good-reads/static/js/runtime~main.56495119.js"
+  }
+]);
